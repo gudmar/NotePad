a702efae2ac7a79eba085f4d0f0ad42675065aea
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-08-10 22:57:14 +0200

    Test for move parent element added and is passing

--- src/app/directives/movable-parent.directive.spec.ts
+++ src/app/directives/movable-parent.directive.spec.ts
@@ -1,8 +1,108 @@
-// import { MovableParentDirective } from './movable-parent.directive';
+import { MovableParentDirective } from './movable-parent.directive';
+import {Component, DebugElement, ElementRef, ViewChild, HostListener,  CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {ComponentFixture, TestBed, tick, fakeAsync} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import { MovablePointComponent } from '../movable-point/movable-point.component'
+import { ConstantPool } from '@angular/compiler';
+import { ConcatSource } from 'webpack-sources';
 
-// describe('MovableParentDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new MovableParentDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
+@Component(
+    {
+      selector: 'test-component-wrapper',
+      template: `
+      <div class = "wrapper">
+        <div class = "this-should-be-moved">
+            <div class = "add-nested-level">
+                <div class = "add-nested-level">
+                    <movable-point movableparent [nestedLevel] = "4" id="grabThisToMoveParent"></movable-point>
+                </div>
+            </div>
+        </div>
+      </div>
+      `,
+      styles: [`
+        .wrapper{
+          position: relative;
+          width: 200vw;
+          height: 200vh;
+        }
+        .this-should-be-moved{
+            position: absolute;
+            width: 50px;
+            height: 50px;
+            background-color: gray;
+        }
+        .add-nested-level{
+            position: relative;
+            width: 100%;
+            height: 100%;
+        }
+      `]
+    })
+  
+    class TestComponent{
+  
+      constructor(){};
+  
+  
+    }
+  
+  
+    class MockElementRef implements ElementRef {
+      nativeElement = {offsetLeft: 0, offsetTop: 0};
+    }
+
+
+describe('MovableParentDirective', () => {
+    let testComponent: TestComponent;
+    let fixture: ComponentFixture<TestComponent>
+    let elementThatShouldBeMoved: HTMLElement;
+    let movingHandle: HTMLElement;
+    let testZoneDocumentReference: HTMLElement;
+    let positionOfNotMovedElement: {x: number, y: number};
+    let getCurrentPosition = function() {return {clientX: elementThatShouldBeMoved.offsetLeft, clientY: elementThatShouldBeMoved.offsetTop}}
+    let getMovementDelta = function(step: number) {return {clientX: step, clientY: step}}
+    let makeMove = function(step: number){
+        movingHandle.dispatchEvent(new MouseEvent('mousedown', getCurrentPosition()));
+        fixture.detectChanges();
+        fixture.whenStable();
+        // debugger;
+        testZoneDocumentReference.dispatchEvent(new MouseEvent('mousemove', getMovementDelta(step)))
+        fixture.detectChanges();
+        fixture.whenStable();
+        testZoneDocumentReference.dispatchEvent(new MouseEvent('mouseup', getMovementDelta(step)))
+        fixture.detectChanges();
+        fixture.whenStable();
+    }
+  
+      beforeEach(async () => {
+        // dummyBody = getDummyBody();
+      fixture = await TestBed.configureTestingModule({
+        declarations: [TestComponent, MovablePointComponent, MovableParentDirective],
+        schemas:      [ CUSTOM_ELEMENTS_SCHEMA ]
+      }).createComponent(TestComponent);
+      fixture.detectChanges();
+      elementThatShouldBeMoved = fixture.nativeElement.querySelector('.wrapper');
+      movingHandle = fixture.nativeElement.querySelector('movable-point');
+      testZoneDocumentReference = fixture.nativeElement.ownerDocument;
+      positionOfNotMovedElement = {x: elementThatShouldBeMoved.offsetLeft, y: elementThatShouldBeMoved.offsetTop};
+    });
+
+  it('should create an instance', () => {
+    let elRef: ElementRef = new MockElementRef();
+    const directive = new MovableParentDirective(elRef);
+    expect(directive).toBeTruthy();
+  });
+
+  it('should move parent element on mousedown, mousemove, mouseup, handler is 3 levels nested', async()=>{
+      console.log('Directive ELEMEnt')
+      console.log(fixture.debugElement.query(By.directive(MovableParentDirective)))
+      console.log('getCURRNE posiont' + getCurrentPosition().clientX + " " + getCurrentPosition().clientY)
+    makeMove(100);
+    let positionAfterMovement = getCurrentPosition();
+    console.log('positionAfterMOvement '  + positionAfterMovement.clientX + ' ' + positionAfterMovement.clientY)
+    console.dir(elementThatShouldBeMoved)
+    expect({x: positionAfterMovement.clientX, y: positionAfterMovement.clientY})
+            .toEqual({x: 100 + positionOfNotMovedElement.x, y: 100 + positionOfNotMovedElement.y});
+  })
+});

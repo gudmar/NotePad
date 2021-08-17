c930c1e3607a92cdcb22447d4a338ede4bf9395a
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-08-17 12:03:24 +0200

    UUID service implemented and tested

--- src/app/services/unique-id-provider.service.spec.ts
+++ src/app/services/unique-id-provider.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UniqueIdProviderService } from './unique-id-provider.service';
+import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
 
 describe('UniqueIdProviderService', () => {
   let service: UniqueIdProviderService;
@@ -13,4 +14,21 @@ describe('UniqueIdProviderService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should create 1 000 000 unique ids', ()=>{
+    let idGeneratorInstance = new UniqueIdProviderService();
+    let uniqueIds:string[] = [];
+    let generateSingleId = function(){
+      return (idGeneratorInstance.getUniqueId())
+    }
+    let checkIfGeneratedIdIsUnique = function(id: string){
+      return uniqueIds.find((element) => {return element == id}) ? false : true
+    }
+    for (let iteration = 0; iteration++; iteration < 1000000){
+      let tempId = generateSingleId();
+      let isUnique = checkIfGeneratedIdIsUnique(tempId);
+      if (isUnique) uniqueIds.push(tempId)
+      expect(isUnique).toBe(true)
+    }
+  })
 });
--- src/app/services/unique-id-provider.service.ts
+++ src/app/services/unique-id-provider.service.ts
@@ -4,10 +4,26 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UniqueIdProviderService {
-
+  uuidsGeneratedSoFar: string[] = [];
   constructor() { }
 
+  checkIfUUIDWasAlreadyGenerated(elementToCheck: string){
+    let findingFunction = function(element: string){ return element === elementToCheck}
+    return (this.uuidsGeneratedSoFar.find(findingFunction) == undefined) ? true : false;
+  }
+
+  getGoodEnoughId(){
+    return Date.now().toString(36) + Math.random().toString(36).substring(2);
+    // https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+  }
+
   getUniqueId(): string{
-    return Math.random() + '';
+    let tempId = '';
+    if (this.uuidsGeneratedSoFar.length > 1000000000) return 'xxxxxxxx'; 
+    // should never happen, there is potential infinite loop, that will occure if too many IDs were generated, but number is huge
+    while (tempId === '' && this.checkIfUUIDWasAlreadyGenerated(tempId)){
+      tempId = this.getGoodEnoughId();
+    }
+    return tempId
   }
 }

c725fcf9b6f13632096cf1f33f5010b6a49d48c2
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-08-20 01:31:34 +0200

    note test made work. Needed mocking communication service

--- src/app/note/note.component.spec.ts
+++ src/app/note/note.component.spec.ts
@@ -2,17 +2,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EventEmitter } from '@angular/core';
 
 import { NoteComponent } from './note.component';
+import { CommunicationService } from '../services/communication.service';
 
-xdescribe('NoteComponent', () => {
+describe('NoteComponent', () => {
   let component: NoteComponent;
   let fixture: ComponentFixture<NoteComponent>;
+  let communicationService;
 
   beforeEach(async () => {
+    communicationService = jasmine.createSpyObj(['inform'])
     await TestBed.configureTestingModule({
-      declarations: [ NoteComponent ]
+      declarations: [ NoteComponent ],
+      providers: [ {provide: CommunicationService, useValue: communicationService}]
     })
     .compileComponents();
   });
+
+  // inform(eventType: string, data: any)
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NoteComponent);

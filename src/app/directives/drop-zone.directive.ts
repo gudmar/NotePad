9a4dffde669362d0f9adddcc87b83adbd7adb64e
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-09-02 00:50:19 +0200

    Loading from file: file dropped down on page element is processed, and content is displayed in console. Dropped functionality will be handeled in directive

---
+++ src/app/directives/drop-zone.directive.ts
@@ -0,0 +1,60 @@
+import { Directive, HostListener } from '@angular/core';
+import { CommunicationService} from '../services/communication.service'
+
+@Directive({
+  selector: '[dropZone]'
+})
+export class DropZoneDirective {
+  // isDraggedOver: boolean = false;
+  constructor(private communicator: CommunicationService) { }
+
+  @HostListener('dragover', ['$event'])
+  onDragOver(event: any){
+    event.stopPropagation();
+    event.preventDefault();
+    event.dataTransfer.gropEffect = 'copy'
+    // this.isDraggedOver = true;
+    // let timeOut = setTimeout(()=>{this.isDraggedOver = false; clearTimeout(timeOut)}, 100)
+  }
+  
+  @HostListener('drop', ['$event'])
+  async onDrop(event: any){
+    // event.stopPropagation();
+    event.preventDefault();
+    let data = event.dataTransfer;
+    if (data.files.length > 1) {
+      this.communicator.inform('informUser', 'Only one file at a time is supported at the moment.')
+      console.log('Only one file at a time supported')
+      return null;
+    } 
+    let dataItem = data.items[0];
+    if (dataItem.kind=='file') {
+      let fileContent = await this.getFileContent(dataItem.getAsFile())
+      console.log(this.decodeFileContent(fileContent));
+    }
+    console.log(data)
+    return null;
+  }
+
+  decodeFileContent(utf8Coded: any){
+    let decoder = new TextDecoder('utf-8')
+    return decoder.decode(utf8Coded)
+  }
+
+  getFileContent(file: any) {
+    var reader = new FileReader()
+    var output = ''
+
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.onload = () => {
+            // resolve(csv2array(decoder.decode(reader.result)))
+            // resolve(decoder.decode(reader.result))
+            resolve(reader.result)
+        }
+        reader.onerror = reject
+        reader.readAsArrayBuffer(file)
+    })
+}
+
+}

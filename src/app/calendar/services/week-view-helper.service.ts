075cb8ded6f00778a6603b534cb8a51c5563111e
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-10-20 22:50:27 +0200

    Next week in week calendar view works fine now

--- src/app/calendar/services/week-view-helper.service.ts
+++ src/app/calendar/services/week-view-helper.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { EventManagerService } from './event-manager.service';
 import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
 import { CalendarObjectProviderService } from './calendar-object-provider.service';
+import { createWriteStream } from 'fs';
 
 @Injectable({
   providedIn: 'root'
@@ -38,74 +39,112 @@ export class WeekViewHelperService {
   }
 
   calculateNextCW(offset: -1 | 1){
-    let plannedCW: number = this.currentCwIndex + offset;
-    let plannedMonth: number = this.currentMonthIndex;
-    let plannedYear: number = this.currentYear;
-    let getObjectToReturn = function() {return {cw: plannedCW, month: plannedMonth, year: plannedYear}}
-    if (offset == -1 && this.currentCwIndex == 1) {
-      plannedMonth = 11; plannedYear = this.currentYear-1; 
-      let lastWeekLastDecember = Math.max(...this.calendarProvider.getCWNumbersForMonth(plannedYear,plannedMonth));
-      // let lastWeekLastDecember = weeksLastDecember[weeksLastDecember.length - 1];
-      plannedCW = lastWeekLastDecember;
-      return getObjectToReturn();
-    }
-    if (this.getArrId(this.cwInCurrentMonth, plannedCW) == -1) {
-      let indexOfCurrnetCW = this.getArrId(this.cwInCurrentMonth, this.currentCwIndex);
-      if (indexOfCurrnetCW == 0) {
-        plannedMonth--;
-        if (plannedMonth < 0) {plannedMonth = 11; plannedYear--;}
-      }
-      if (indexOfCurrnetCW == this.cwInCurrentMonth.length - 1) {
-        plannedMonth+1; //++;
-        if (plannedMonth > 11) {plannedMonth = 0; plannedYear+1; plannedCW = 1;}//++
-      }
-    } else {
-      return getObjectToReturn();
-    }
-    return getObjectToReturn();
+    let calculator = new nextCwCalculator(
+      this.currentCwIndex, this.currentMonthIndex, this.currentYear, this.calendarProvider
+    )
+    return calculator.calculateNextCW(offset)
   }
 
   // calculateNextCW(offset: -1 | 1){
   //   let plannedCW: number = this.currentCwIndex + offset;
   //   let plannedMonth: number = this.currentMonthIndex;
   //   let plannedYear: number = this.currentYear;
+  //   let decrease1CWJanuary = offset == -1 && this.currentCwIndex == 1;
+  //   let plannedCwDoesNotBelongToCurrentMonth = this.getArrId(this.cwInCurrentMonth, plannedCW) == -1;
   //   let getObjectToReturn = function() {return {cw: plannedCW, month: plannedMonth, year: plannedYear}}
-  //   let doesMonthBelongToYear = function(monthIndex: number){
-  //     return (monthIndex < 0 || monthIndex > 11)?false:true
+  //   if (decrease1CWJanuary) {
+  //     plannedMonth = 11; plannedYear = this.currentYear-1; 
+  //     let lastWeekLastDecember = Math.max(...this.calendarProvider.getCWNumbersForMonth(plannedYear,plannedMonth));
+  //     plannedCW = lastWeekLastDecember;
+  //     return getObjectToReturn();
   //   }
-  //   if(this.doesCwBelongToCurrnetMonth(plannedCW)) return getObjectToReturn();
-  //   debugger
-  //   plannedMonth = this.getCorrectedMonthId(plannedCW);
-  //   debugger
-  //   plannedYear = plannedYear + this.getYearCorrectionOffset(plannedCW);
+  //   if (plannedCwDoesNotBelongToCurrentMonth) {
+  //     let indexOfCurrnetCW = this.getArrId(this.cwInCurrentMonth, this.currentCwIndex);
+  //     if (indexOfCurrnetCW == 0) {
+  //       plannedMonth--;
+  //       if (plannedMonth < 0) {plannedMonth = 11; plannedYear--;}
+  //     }
+  //     if (indexOfCurrnetCW == this.cwInCurrentMonth.length - 1) {
+  //       plannedMonth = plannedMonth+1;
+  //       if (plannedMonth > 11) {plannedMonth = 0; plannedYear = plannedYear+1; plannedCW = 1;}//++
+  //     }
+  //   } else {
+  //     return getObjectToReturn();
+  //   }
   //   return getObjectToReturn();
-
   // }
 
-  // private doesCwBelongToCurrnetMonth(cwIndex: number):boolean{
-  //   return this.cwInCurrentMonth.findIndex((element)=>{return element == cwIndex})==-1?false: true;
-  // }
-  // private getPlannedMonthIndex(plannedCW: number){
-  //   if (plannedCW > this.cwInCurrentMonth[this.cwInCurrentMonth.length]) return this.currentMonthIndex+1;
-  //   if (this.getArrId(this.cwInCurrentMonth, plannedCW) != -1) return this.currentMonthIndex;
-
-  //   return  (plannedCW > Math.max(...this.cwInCurrentMonth))?this.currentMonthIndex+1:this.currentCwIndex-1;
-  // }
   private getArrId(arr:number[],valueToSearch: number){
     return arr.findIndex((e)=>{return e == valueToSearch})
   }
-  // private getCorrectedMonthId(plannedCW: number){
-  //   let plannedMonthId = this.getPlannedMonthIndex(plannedCW);
-  //   debugger;
-  //   if (plannedMonthId < 0) return 11;
-  //   if (plannedMonthId > 11) return 0;
-  //   return plannedMonthId;
-  // }
+}
 
-  // private getYearCorrectionOffset(plannedCW: number){
-  //   let plannedMonthId = this.getPlannedMonthIndex(plannedCW);
-  //   if (plannedMonthId > 11) return 1;
-  //   if (plannedMonthId < 0) return -1;
-  //   return 0;
-  // }
+
+class nextCwCalculator{
+  currentCwIndex:number;
+  currentMonthIndex:number;
+  currentYear:number;
+  plannedCW:number;
+  plannedMonth:number;
+  plannedYear:number;
+  constructor(currentCwIndex:number, 
+    currentMonthIndex:number, 
+    currentYear:number,
+    private calendarProvider: CalendarObjectProviderService
+  ){
+    this.currentCwIndex = currentCwIndex;
+    this.currentMonthIndex = currentMonthIndex;
+    this.currentYear = currentYear;
+    this.plannedCW = currentCwIndex;
+    this.plannedMonth = currentMonthIndex;
+    this.plannedYear = currentYear;
+  }
+  
+  calculateNextCW(offset: -1 | 1){
+    this.plannedCW = this.plannedCW + offset;
+    
+    if (this.actionIsToDecrease1CWJanuary(offset)) {
+      this.plannedMonth = 11; this.plannedYear = this.currentYear-1; 
+      let lastWeekOfDecember = Math.max(...this.getCwIndexesForMonth(11,this.plannedYear));
+      this.plannedCW = lastWeekOfDecember;
+      return this.getPlannedCwData();
+    }
+    if (this.plannedCwDoesNotBelongToCurrentMonth(this.plannedCW)) {
+      if (this.isFirstCwOfCurrentMonth(this.currentCwIndex)) {
+        this.plannedMonth = this.plannedMonth - 1;
+        if (this.plannedMonth < 0) {this.plannedMonth = 11; this.plannedYear = this.plannedYear - 1;}
+      }
+      if (this.isLastCwOfCurrentMonth(this.currentCwIndex)) {
+        this.plannedMonth = this.plannedMonth + 1;
+        if (this.plannedMonth > 11) {this.plannedMonth = 0; this.plannedYear = this.plannedYear+1; this.plannedCW = 1;}
+      }
+    } else {
+      return this.getPlannedCwData();
+    }
+    return this.getPlannedCwData();
+  }
+
+  private getCwIndexesForMonth(month:number, year:number){
+    return this.calendarProvider.getCWNumbersForMonth(year,month)
+  };
+
+  get cwInCurrentMonth(){return this.getCwIndexesForMonth(this.currentMonthIndex, this.currentYear)}
+
+  private actionIsToDecrease1CWJanuary(offset:-1|1){return offset == -1 && this.currentCwIndex == 1;}
+  private plannedCwDoesNotBelongToCurrentMonth(plannedCw:number) {return this.getArrId(this.cwInCurrentMonth, plannedCw) == -1};
+
+  private getPlannedCwData(){return {cw: this.plannedCW, month: this.plannedMonth, year: this.plannedYear}}
+
+  private isFirstCwOfCurrentMonth(cwIndex:number){
+    let indexOfCurrnetCW = this.getArrId(this.cwInCurrentMonth, cwIndex);
+    return indexOfCurrnetCW == 0;
+  }
+  private isLastCwOfCurrentMonth(cwIndex:number){
+    let indexOfCurrnetCW = this.getArrId(this.cwInCurrentMonth, cwIndex);
+    return indexOfCurrnetCW == this.cwInCurrentMonth.length - 1 
+  }
+
+  private getArrId(arr:number[],valueToSearch: number){
+    return arr.findIndex((e)=>{return e == valueToSearch})
+  }
 }
